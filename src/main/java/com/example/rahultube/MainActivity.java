08504c8a9b29163519901d638ed936a239930843
Heytarun <72151546+Heytarun@users.noreply.github.com>
2020-09-30 21:03:49 +0530

    Add files via upload

---
+++ src/main/java/com/example/rahultube/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.rahultube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView clip = findViewById(R.id.videoclip);
+        clip.setVideoPath("android.resource://"+ getPackageName() + "/" + R.raw.clip);
+        MediaController mediaController  = new MediaController(this);
+        clip.setMediaController(mediaController);
+        mediaController.setAnchorView(clip);
+        clip.start();
+    }
+}
